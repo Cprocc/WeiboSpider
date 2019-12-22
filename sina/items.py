cd219a9b5b322e41ef5b2bda7c0a97ec7c23b99e
Cprocc <755516806@qq.com>
2019-12-22 19:56:16 +0800

    修改为指定微博的 评论/点赞/转发

--- sina/items.py
+++ sina/items.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from scrapy import Item, Field
 
 
@@ -12,15 +11,10 @@ class TweetsItem(Item):
     comment_num = Field()  # 评论数
     content = Field()  # 微博内容
     user_id = Field()  # 发表该微博用户的id
-    tool = Field()  # 发布微博的工具
-    image_url = Field()  # 图片
-    video_url = Field()  # 视频
-    location_map_info = Field() # 定位的经纬度信息
-    origin_weibo = Field()  # 原始微博，只有转发的微博才有这个字段
     crawl_time = Field()  # 抓取时间戳
 
 
-class InformationItem(Item):
+class FansInformationItem(Item):
     """ 个人信息 """
     _id = Field()  # 用户ID
     nick_name = Field()  # 昵称
@@ -37,8 +31,8 @@ class InformationItem(Item):
     vip_level = Field()  # 会员等级
     authentication = Field()  # 认证
     person_url = Field()  # 首页链接
-    labels = Field()  # 标签
     crawl_time = Field()  # 抓取时间戳
+    labels = Field()  # 标签
 
 
 class RelationshipsItem(Item):
@@ -47,6 +41,27 @@ class RelationshipsItem(Item):
     fan_id = Field()  # 关注者,即粉丝的id
     followed_id = Field()  # 被关注者的id
     crawl_time = Field()  # 抓取时间戳
+
+
+class CommentsInformationItem(Item):
+    """ 个人信息 """
+    _id = Field()  # 用户ID
+    nick_name = Field()  # 昵称
+    gender = Field()  # 性别
+    province = Field()  # 所在省
+    city = Field()  # 所在城市
+    brief_introduction = Field()  # 简介
+    birthday = Field()  # 生日
+    tweets_num = Field()  # 微博数
+    follows_num = Field()  # 关注数
+    fans_num = Field()  # 粉丝数
+    sex_orientation = Field()  # 性取向
+    sentiment = Field()  # 感情状况
+    vip_level = Field()  # 会员等级
+    authentication = Field()  # 认证
+    person_url = Field()  # 首页链接
+    crawl_time = Field()  # 抓取时间戳
+    labels = Field()  # 标签
 
 
 class CommentItem(Item):
@@ -58,5 +73,70 @@ class CommentItem(Item):
     content = Field()  # 评论的内容
     weibo_url = Field()  # 评论的微博的url
     created_at = Field()  # 评论发表时间
-    like_num = Field()  # 点赞数
+    crawl_time = Field()  # 抓取时间戳
+
+
+class RepostInformationItem(Item):
+    """ 个人信息 """
+    _id = Field()  # 用户ID
+    nick_name = Field()  # 昵称
+    gender = Field()  # 性别
+    province = Field()  # 所在省
+    city = Field()  # 所在城市
+    brief_introduction = Field()  # 简介
+    birthday = Field()  # 生日
+    tweets_num = Field()  # 微博数
+    follows_num = Field()  # 关注数
+    fans_num = Field()  # 粉丝数
+    sex_orientation = Field()  # 性取向
+    sentiment = Field()  # 感情状况
+    vip_level = Field()  # 会员等级
+    authentication = Field()  # 认证
+    person_url = Field()  # 首页链接
+    crawl_time = Field()  # 抓取时间戳
+    labels = Field()  # 标签
+
+
+class RepostItem(Item):
+    """
+    微博评论信息
+    """
+    _id = Field()
+    rep_user_id = Field()  # 评论用户的id
+    content = Field()  # 评论的内容
+    weibo_url = Field()  # 评论的微博的url
+    created_at = Field()  # 评论发表时间
+    created_device = Field()  # 转发设备
+    crawl_time = Field()  # 抓取时间戳
+
+
+class LikeInformationItem(Item):
+    """ 个人信息 """
+    _id = Field()  # 用户ID
+    nick_name = Field()  # 昵称
+    gender = Field()  # 性别
+    province = Field()  # 所在省
+    city = Field()  # 所在城市
+    brief_introduction = Field()  # 简介
+    birthday = Field()  # 生日
+    tweets_num = Field()  # 微博数
+    follows_num = Field()  # 关注数
+    fans_num = Field()  # 粉丝数
+    sex_orientation = Field()  # 性取向
+    sentiment = Field()  # 感情状况
+    vip_level = Field()  # 会员等级
+    authentication = Field()  # 认证
+    person_url = Field()  # 首页链接
+    crawl_time = Field()  # 抓取时间戳
+    labels = Field()  # 标签
+
+
+class LikeItem(Item):
+    """
+    微博评论信息
+    """
+    _id = Field()
+    like_user_id = Field()  # 评论用户的id
+    weibo_url = Field()  # 评论的微博的url
+    created_at = Field()  # 评论发表时间
     crawl_time = Field()  # 抓取时间戳
--- sina/pipelines.py
+++ sina/pipelines.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 import pymongo
 from pymongo.errors import DuplicateKeyError
-from sina.items import RelationshipsItem, TweetsItem, InformationItem, CommentItem
+from sina.items import RelationshipsItem, TweetsItem, FansInformationItem, CommentItem, CommentsInformationItem, \
+    RepostItem, RepostInformationItem, LikeItem, LikeInformationItem
 from sina.settings import LOCAL_MONGO_HOST, LOCAL_MONGO_PORT, DB_NAME
 
 
@@ -9,21 +9,37 @@ class MongoDBPipeline(object):
     def __init__(self):
         client = pymongo.MongoClient(LOCAL_MONGO_HOST, LOCAL_MONGO_PORT)
         db = client[DB_NAME]
-        self.Information = db["Information"]
+        self.Information = db["Fans_Information"]
         self.Tweets = db["Tweets"]
         self.Comments = db["Comments"]
         self.Relationships = db["Relationships"]
+        self.Comments_Information = db["Comments_Information"]
+        self.Repost = db["Repost"]
+        self.Repost_Informations = db["Repost_Informations"]
+        self.Like = db["Like"]
+        self.Like_Informations = db["Like_Informations"]
 
     def process_item(self, item, spider):
         """ 判断item的类型，并作相应的处理，再入数据库 """
         if isinstance(item, RelationshipsItem):
             self.insert_item(self.Relationships, item)
-        elif isinstance(item, TweetsItem):
-            self.insert_item(self.Tweets, item)
-        elif isinstance(item, InformationItem):
-            self.insert_item(self.Information, item)
+        # elif isinstance(item, TweetsItem):
+        #     self.insert_item(self.Tweets, item)
+        # elif isinstance(item, FansInformationItem):
+        #     self.insert_item(self.Information, item)
+
         elif isinstance(item, CommentItem):
             self.insert_item(self.Comments, item)
+        elif isinstance(item, CommentsInformationItem):
+            self.insert_item(self.Comments_Information, item)
+        elif isinstance(item, RepostItem):
+            self.insert_item(self.Repost, item)
+        elif isinstance(item, RepostInformationItem):
+            self.insert_item(self.Repost_Informations, item)
+        elif isinstance(item, LikeItem):
+            self.insert_item(self.Like, item)
+        elif isinstance(item, LikeInformationItem):
+            self.insert_item(self.Like_Informations, item)
         return item
 
     @staticmethod
